47b42e6b81c658d590c4dedec726a099d3f922cc
raz0229 <raja.zafar229@gmail.com>
2022-07-03 13:00:01 +0500

    get duration and create file

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from youtube_transcript_api import YouTubeTranscriptApi as yt
+import scrapetube
 
+CHANNEL_ID="UCMiJRAwDNSNzuYeN2uWa0pA"
+SEARCH_STRING="sub to the channel"
+
+# https://commentpicker.com/youtube-channel-id.php
+videos = scrapetube.get_channel(CHANNEL_ID)
 #video_id = input("Enter Video ID: ")
+f = open(f"url_list_{CHANNEL_ID}.txt", "a")    
 # AaxPLnvCy08 (video with no given text)
 
 def getDurationOfText(text, video_id):
@@ -9,9 +16,15 @@ def getDurationOfText(text, video_id):
     return (int(result[0]['start']) + 1)
 
 if __name__ == '__main__':
-    id = 'Wuq7lS9rljw'
-    try:
-        start_time = getDurationOfText('sub to the channel', video_id=id)
-        print(f'https://www.youtube.com/watch?v={id}&t={start_time}s')
-    except Exception:
-        print('Search substring not found in video ID: ', id)
+    for video in videos:
+        try:
+            id = video['videoId']
+            print('\nWriting \'{}\' to file: url_list_{}.txt'.format(id, CHANNEL_ID))
+            start_time = getDurationOfText(SEARCH_STRING, video_id=id)
+            f.write(f'https://www.youtube.com/watch?v={id}&t={start_time}s\n')
+        except Exception:
+            print('[MESSAGE] Search substring not found in video ID: {} Skipping...'.format(id))
+            continue
+    f.close()
+    print('\n[MESSAGE] Created file: url_list_{}.txt in current directory.'.format(CHANNEL_ID))
+    
