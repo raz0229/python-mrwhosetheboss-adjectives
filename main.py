ee997642c99dd0c128576376f80dc21eb458ce14
raz0229 <raja.zafar229@gmail.com>
2022-07-03 17:28:03 +0500

    url list finished

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from youtube_transcript_api import YouTubeTranscriptApi as yt
 import scrapetube
+import os
 
 # CONFIGURATION
 CHANNEL_ID="UCMiJRAwDNSNzuYeN2uWa0pA"
@@ -23,7 +24,7 @@ if __name__ == '__main__':
         try:
             id = video['videoId']
             
-            print('\nWriting \'{}\' to file: url_list_{}.txt'.format(id, CHANNEL_ID))
+            print('\nWriting \'{}\' to file: url_list.txt')
             current = getDurationOfTextAndNextLines(SEARCH_STRING, video_id=id)
             start_time = current[0]
             arr = current[1].replace('would be ', '').split()
@@ -31,9 +32,13 @@ if __name__ == '__main__':
             # writing 2 words from script in case there is a degree adverb
             adjective = f"{arr[0]}" if len(arr) == 1 else f"{arr[0]}-{arr[1]}"
             f.write(f'{adjective} https://www.youtube.com/watch?v={id}&t={start_time}s\n')
+
+            # Force the OS to store the file buffer to disc
+            f.flush()
+            os.fsync(f.fileno())
         except Exception:
             print('[MESSAGE] Search substring not found in video ID: {}\nSkipping...'.format(id))
             continue
     f.close()
-    print('\n[MESSAGE] Created file: url_list_{}.txt in current directory.'.format(CHANNEL_ID))
+    print('\n[MESSAGE] Created file: url_list.txt in current directory.')
     
