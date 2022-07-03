274fcc962e324177b5854dd951fcd4f29cbdfef9
raz0229 <raja.zafar229@gmail.com>
2022-07-03 14:01:48 +0500

    write adjective with url

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from youtube_transcript_api import YouTubeTranscriptApi as yt
 import scrapetube
 
+# CONFIGURATION
 CHANNEL_ID="UCMiJRAwDNSNzuYeN2uWa0pA"
 SEARCH_STRING="sub to the channel"
 
@@ -10,20 +11,28 @@ videos = scrapetube.get_channel(CHANNEL_ID)
 f = open(f"url_list_{CHANNEL_ID}.txt", "a")    
 # AaxPLnvCy08 (video with no given text)
 
-def getDurationOfText(text, video_id):
+def getDurationOfTextAndNextLines(text, video_id):
     transcript = yt.get_transcript(video_id) #'Wuq7lS9rljw'
-    result = list(filter(lambda e: text in e['text'].lower(), transcript))
-    return (int(result[0]['start']) + 1)
+    result = list(filter(lambda e: text in e[1]['text'].lower(), enumerate(transcript)))
+    temp = result[0]
+    concat = transcript[int(temp[0]) + 1]['text'] + ' ' + transcript[int(temp[0]) + 2]['text']
+    return (int(temp[1]['start']) + 1, concat)
 
 if __name__ == '__main__':
     for video in videos:
         try:
             id = video['videoId']
+            
             print('\nWriting \'{}\' to file: url_list_{}.txt'.format(id, CHANNEL_ID))
-            start_time = getDurationOfText(SEARCH_STRING, video_id=id)
-            f.write(f'https://www.youtube.com/watch?v={id}&t={start_time}s\n')
+            current = getDurationOfTextAndNextLines(SEARCH_STRING, video_id=id)
+            start_time = current[0]
+            arr = current[1].replace('would be ', '').split()
+
+            # writing 2 words from script in case there is a degree adverb
+            adjective = f"{arr[0]}" if len(arr) == 1 else f"{arr[0]}-{arr[1]}"
+            f.write(f'{adjective} https://www.youtube.com/watch?v={id}&t={start_time}s\n')
         except Exception:
-            print('[MESSAGE] Search substring not found in video ID: {} Skipping...'.format(id))
+            print('[MESSAGE] Search substring not found in video ID: {}\nSkipping...'.format(id))
             continue
     f.close()
     print('\n[MESSAGE] Created file: url_list_{}.txt in current directory.'.format(CHANNEL_ID))
